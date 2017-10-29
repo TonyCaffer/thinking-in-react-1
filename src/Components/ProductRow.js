312449bcfa32ebcdc5db8a3ae71edd5f356129a2
TonyCaffer <tonycaffer82@gmail.com>
2017-10-29 13:53:24 +0000

    Added shouldComponentUpdate to ProductRow. Rendering is now fast in production build, still slow on dev build. I'll consider that a success and make sure I never have 5000 list components on screen.

--- src/Components/ProductRow.js
+++ src/Components/ProductRow.js
@@ -1,18 +1,19 @@
 import React from "react";
 
 class ProductRow extends React.Component {
-	constructor(props) {
-		super(props);
-		this.onChangePrice = (e) => {
-			e.preventDefault;
-			this.props.onChangePrice(this.props.product.guid, e.target.value);
-		};
+	
+	onChangePrice = (e) => {
+		e.preventDefault;
+		this.props.onChangePrice(this.props.product.guid, e.target.value);
 	}
+
+	shouldComponentUpdate = (nextProps) => this.props.product.price !== nextProps.product.price;
+
 	render() {
 		const product = this.props.product;
 		const name = product.isInStock ? product.name : <span style={{color:'red'}}>{product.name}</span>;
 		
-		// console.log("Render:" + product.guid); // Fires when a single product price is changed
+		console.log("Render:" + product.guid); // Fires when a single product price is changed
 
 		return (
 			<tr key={product.guid}>
